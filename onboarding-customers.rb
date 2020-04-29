d9452515df08a5a2567fc4f87479d57975643747
Dan <dan.lewis@bamboocreative.nz>
2020-04-29 19:07:02 +1200

    update to SF fields

--- onboarding-customers.rb
+++ onboarding-customers.rb
@@ -26,86 +26,184 @@ end
 while i <= max do
 
 
-# Account Name#
-# Person Account: Title#
-# Person Account: First Name#
-# Person Account: Last Name#
-# UDID#
-# Device Type#
-# Accepted T&C#
-# TroveAccountId#
-# Person Account: Mobile#
-# Person Account: Email#
-# Birth Date#
-# Person Account: Mailing Street#
-# Person Account: Mailing Address Line1#
-# Person Account: Mailing Address Line2#
-# Person Account: Mailing Address Line3#
-# Person Account: Mailing City#
-# Person Account: Mailing State/Province#
-# Person Account: Mailing Zip/Postal Code#
-# Person Account: Mailing Country#
-# Address Lookup#
-# Age Income#
-# Confirm Investments#
-# Email Pass#
-# First Deposit#
-# Goal Reach#
-# Goal Split#
-# Id Method#
-# Id Pending#
-# Id Verified#
-# Personal Details#
-# Select Goals#
-# Terms#
-# Email Verified#
-# Income#
-# IRD Number#
-# PIR#
-# Risk Profile#
-# Tax Resident#
-# Person Account: Email Opt Out#
-# Description#
-# Person Account: Mobile Opt Out#
-# Account Source#
-# Account Record Type#
+  # Account Name
+  firstName = FFaker::NameIT.first_name
+  lastName = FFaker::NameIT.last_name
 
-  line << i.to_s
+  line << firstName + ' ' + lastName
   line << delimiter
 
-  line << fn = FFaker::NameIT.first_name
+  # Person Account: Title
+  titles = ['Mr','Ms','Dr','Don']
+  line = titles.sample
   line << delimiter
 
-  line << ln = FFaker::NameIT.last_name
+  # Person Account: First Name
+  line << firstName
   line << delimiter
 
-  line << FFaker::AddressAU.street_name
+  # Person Account: Last Name
+  line << lastName
   line << delimiter
 
-  number = random_normal_dist(0, 30, -250, 250).ceil.abs
-  line << number.to_s
+  # UDID
+  line << fn = FFaker::Guid.guid
   line << delimiter
 
-  line << FFaker::AddressAU.zip_code
+  # Device Type
+  deviceType = ['IOS','Android']
+  line << deviceType.sample
   line << delimiter
 
-  line << FFaker::AddressAU.city
+  # Accepted T&C
+  line << fn = FFaker::Boolean.random.to_s
   line << delimiter
 
-  line << FFaker::Internet.email("#{fn}.#{ln}")
+  # TroveAccountId
+  line << fn = FFaker::Guid.guid
   line << delimiter
 
-
-  height = random_normal_dist(175, 15, 120, 220)
-  line << height.round.to_s
+  # Person Account: Mobile
+  line << fn = FFaker::PhoneNumberAU.mobile_phone_number
   line << delimiter
 
-  weight = random_normal_dist(height-100, 10, 40, 180)
-  line << weight.round.to_s
+  # Person Account: Email
+  line << fn = FFaker::Internet.safe_email
   line << delimiter
 
+  # Birth Date
   birthday = Date.today - rand(365*70) - 16*365
   line << birthday.to_s
+  line << delimiter
+
+  # Person Account: Mailing Street
+  buildingNumber = FFaker::AddressAU.building_number
+  street = FFaker::AddressAU.street_name
+  line << buildingNumber + ' ' + street
+  line << delimiter
+
+  # Person Account: Mailing Address Line1
+  line << fn = FFaker::AddressAU.secondary_address
+  line << delimiter
+
+  # Person Account: Mailing Address Line2
+  line << fn = FFaker::AddressAU.suburb
+  line << delimiter
+
+  # Person Account: Mailing City
+  line << fn = FFaker::AddressAU.city
+  line << delimiter
+
+  # Person Account: Mailing State/Province
+  line << fn = FFaker::AddressAU.state
+  line << delimiter
+
+  # Person Account: Mailing Zip/Postal Code
+  line << fn = FFaker::AddressAU.postcode
+  line << delimiter
+
+  # Person Account: Mailing Country
+  line << fn = FFaker::AddressAU.country
+  line << delimiter
+
+  # Address Lookup
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Age Income
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Confirm Investments
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Email Pass
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # First Deposit
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Goal Reach
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Goal Split
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Id Method
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Id Pending
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Id Verified
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Personal Details
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Select Goals
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Terms
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Email Verified
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Income
+  line << rand(30000..250000).to_s
+  line << delimiter
+
+  # IRD Number
+  line << rand.to_s[2..9]
+  line << delimiter
+
+  # PIR
+  pirRate = [0.105, 0.175, 0.28]
+  line << pirRate.sample.to_s
+  line << delimiter
+
+  # Risk Profile
+  line << rand(1..5)
+  line << delimiter
+
+  # Tax Resident
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Person Account: Email Opt Out
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Description
+  cheesy = FFaker::CheesyLingo.word
+  title = FFaker::Book.title
+  line << cheesy + ' ' + title
+  line << delimiter
+
+  # Person Account: Mobile Opt Out
+  line << fn = FFaker::Boolean.random.to_s
+  line << delimiter
+
+  # Account Source
+  source = ['Website','Adviser']
+  line << source.sample
+  line << delimiter
+
+  # Account Record Type
+  line << 'On-boarding'
 
   puts line
   line = ''
